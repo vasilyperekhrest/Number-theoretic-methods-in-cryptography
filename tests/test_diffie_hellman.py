01386db58f884b0385a1a199e930f6450421d7cb
Vasily Perekhrest <perekhrest-vasily@mail.ru>
2021-07-04 23:21:30 +0700

    Update tests

--- tests/test_diffie_hellman.py
+++ tests/test_diffie_hellman.py
@@ -1,36 +1,27 @@
+import pytest
+
 from ntmcrypt import diffie_hellman
 
 
-def main():
-    p, g = diffie_hellman.gen_public_shared_keys(120)
-    print(f"Public shared keys:\n"
-          f"p = {p}\n"
-          f"g = {g}\n")
+@pytest.mark.parametrize("num_bits", [50,
+                                      60,
+                                      70,
+                                      80,
+                                      90,
+                                      100])
+def test_diffie_hellman(num_bits):
+    p, g = diffie_hellman.gen_public_shared_keys(num_bits)
 
     # A
     pub_a, pr_a = diffie_hellman.gen_keys(p, g)
-    print(f"User: A\n"
-          f"Public key = {pub_a}\n"
-          f"Private key = {pr_a}\n")
 
     # B
     pub_b, pr_b = diffie_hellman.gen_keys(p, g)
-    print(f"User: B\n"
-          f"Public key = {pub_b}\n"
-          f"Private key = {pr_b}\n")
 
     # A -> B
     shared_key_a = diffie_hellman.create_private_shared_key(pub_b, pr_a, p)
-    print(f"A -> B\n"
-          f"User A:\n"
-          f"Private shared key = {shared_key_a}\n")
 
     # B -> A
     shared_key_b = diffie_hellman.create_private_shared_key(pub_a, pr_b, p)
-    print(f"B -> A\n"
-          f"User B:\n"
-          f"Private shared key = {shared_key_b}\n")
 
-
-if __name__ == '__main__':
-    main()
+    assert shared_key_a == shared_key_b
